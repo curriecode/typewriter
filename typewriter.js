4595335162c8014e5e8f1255023b4b8b6d729c4b
curriecode <melaniecurrie01@gmail.com>
2019-09-24 14:37:11 -0700

    "function prints message to console in typewriter animation"

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+let i = 0;
+for (const char of (sentence + ‘\n’)) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, (i++) * 50);
+}
